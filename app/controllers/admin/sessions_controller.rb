6284a3d745510f913b17b76a69fcb7291b09d0e6
rangelo00 <reyes.ralphangelo@gmail.com>
2024-11-11 15:05:36 +0800

    Admin scoped sign-in working

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,9 +1,9 @@
 class Admin::SessionsController < Devise::SessionsController
-    def after_sign_in(resource)
+    def after_sign_in_path_for(resource)
         admin_dashboard_index_path
     end
 
-    def after_sign_out(resource)
+    def after_sign_out_path_for(resource)
         destroy_admin_session_path
     end
   end
