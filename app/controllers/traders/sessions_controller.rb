df8c68e7842ce305c4b4275fb0cc772ce488a663
rangelo00 <reyes.ralphangelo@gmail.com>
2024-11-12 20:41:10 +0800

    Auth for Admin/Trader scoped. Working. Now to implement account priveledges. No home screen yet. Need to manually route {127.0.0.1/*account*/sign-in} Fixed Trader sessionpath when signing in

--- app/controllers/traders/sessions_controller.rb
+++ app/controllers/traders/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Traders::SessionsController < Devise::SessionsController
     def after_sign_in_path_for(resource)
-        trader_dashboard_index_path
+        traders_dashboard_index_path
     end
 
     def after_sign_out_path_for(resource)
